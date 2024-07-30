3d7e7a8c76cfb7c6617d369432739323a361c4f4
dockercaptain <33228193+dockercaptain@users.noreply.github.com>
2024-07-30 18:48:09 +0530

    Add files via upload

---
+++ http.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type Applications struct {
+	Id          int32
+	AppName     string
+	Description string
+	ImageName   string
+}
+
+type AppVersionsResponse struct {
+	Status      string
+	AppVersions []string
+}
+
+type Environment struct {
+	Id          int32
+	Name        string
+	Description string
+}
+
+type PostgresApp struct {
+	Id          int32
+	Status      string
+	Description string
+	ServerName  string
+	AdminUser   string
+	Password    string
+	Version     string
+	Environment string
+	SizeDisk    string
+	StorageType string
+	SizeCPU     string
+	SizeMemory  string
+}
+
+type ErrorResponse struct {
+	Message    string
+	StatusCode string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /marketplace/apps", GetApplications)
+	fmt.Println("localhost:8080...")
+	mux.HandleFunc("GET /marketplace/apps/versions/{id}", GetApplicationVersions)
+	mux.HandleFunc("GET /marketplace/apps/envs", GetEnvironments)
+	mux.HandleFunc("GET /marketplace/apps/{appName}", GetAllAppsDetailsByName)
+	http.ListenAndServe(":8080", mux)
+
+}
+
+// 1.
+func GetApplications(w http.ResponseWriter, r *http.Request) {
+	// Dynamically access the path variable
+	//fmt.Fprintf(w, "Retrieving item with ID: %s", item)
+	applications := GetApplicationsFromDB()
+	json.NewEncoder(w).Encode(applications)
+
+}
+
+// 1.
+func GetApplicationVersions(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Println(id)
+	i, _ := strconv.Atoi(id)
+	versions := GetApplicationVersionsFromID(i)
+	appVersions := BuildResponseForVersions(versions)
+	//fmt.Println(appVersions)
+	res := AppVersionsResponse{
+		Status:      "Success",
+		AppVersions: appVersions,
+	}
+	fmt.Println(res)
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func GetApplicationVersionsFromID(id int) map[string]string {
+	conn, err := GetPostgresConnection()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var versions map[string]string
+	err = conn.QueryRow(context.Background(), `select versions from "applicationDetails" where id = $1`, id).Scan(&versions)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	}
+	//fmt.Println("\n\n", versions)
+	return versions
+}
+
+func BuildResponseForVersions(versionsMap map[string]string) []string {
+	var appVersions []string
+	for _, j := range versionsMap {
+		appVersions = append(appVersions, j)
+	}
+	return appVersions
+}
+
+// 2.
+func GetApplicationsFromDB() []Applications {
+	conn, err := GetPostgresConnection()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var apps []Applications
+	rows, err := conn.Query(context.Background(), `select "appName",id,description,"imageName" from "applicationDetails"`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var appName string
+		var id int32
+		var description string
+		var imageName string
+		err = rows.Scan(&appName, &id, &description, &imageName)
+		if err != nil {
+			fmt.Println(err)
+		}
+		app := Applications{
+			AppName:     appName,
+			Id:          id,
+			Description: description,
+			ImageName:   imageName,
+		}
+		apps = append(apps, app)
+	}
+	return apps
+}
+
+func GetEnvironmentsFromDB() ([]Environment, *ErrorResponse) {
+	conn, _ := GetPostgresConnection()
+	if conn != nil {
+		var envs []Environment
+		rows, err := conn.Query(context.Background(), `select id, name, description from environment_details`)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var name string
+			var id int32
+			var description string
+			err = rows.Scan(&id, &name, &description)
+			if err != nil {
+				fmt.Println(err)
+			}
+			env := Environment{
+				Name:        name,
+				Id:          id,
+				Description: description,
+			}
+			envs = append(envs, env)
+		}
+
+		return envs, nil
+	} else {
+		errorResponse := ErrorResponse{
+			Message:    "Something went wrong, please try again later",
+			StatusCode: "500",
+		}
+		return nil, &errorResponse
+	}
+}
+
+func GetEnvironments(w http.ResponseWriter, r *http.Request) {
+	// Dynamically access the path variable
+	//fmt.Fprintf(w, "Retrieving item with ID: %s", item)
+	environments, err := GetEnvironmentsFromDB()
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(environments)
+	}
+}
+
+func GetAllAppsDetailsByName(w http.ResponseWriter, r *http.Request) {
+	appName := r.PathValue("appName")
+	var postgresList []PostgresApp
+	if appName == "postgres" {
+		postgresList = GetAllPostgresFromDB()
+	}
+	json.NewEncoder(w).Encode(postgresList)
+}
+
+func GetAllPostgresFromDB() ([]PostgresApp, ErrorResponse) {
+	conn, err := GetPostgresConnection()
+	if conn != nil {
+		defer conn.Close(context.Background())
+		var postgresList []PostgresApp
+		rows, err := conn.Query(context.Background(), `select * from installed_postgres_details`)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var id int32
+			var status string
+			var description string
+			var serverName string
+			var adminUser string
+			var password string
+			var version string
+			var environment string
+			var sizeDisk string
+			var storageType string
+			var sizeCPU string
+			var sizeMemory string
+			err = rows.Scan(&id, &status, &description, &serverName, &adminUser, &password, &version, &environment, &sizeDisk, &storageType, &sizeCPU, &sizeMemory)
+			if err != nil {
+				fmt.Println(err)
+			}
+			postgres := PostgresApp{
+				Id:          id,
+				ServerName:  serverName,
+				Description: description,
+				Status:      status,
+			}
+			postgresList = append(postgresList, postgres)
+		}
+		return postgresList, nil
+	} else {
+
+	}
+}
+
+func GetPostgresConnection() (*pgx.Conn, error) {
+	DATABASE_URL := "postgres://postgres:mysecretpassword@192.168.1.41:5432/marketplace"
+	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return nil, errors.New("unable to connect to database")
+	}
+	//defer conn.Close(context.Background())
+	return conn, nil
+}
