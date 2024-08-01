d48bfc87d6771fa0089a0185e12f4e973f31da7e
NaveenKashyapR <48998706+NaveenKashyapR@users.noreply.github.com>
2024-08-01 18:43:07 +0530

    Create postgre-struct.go

---
+++ postgre-struct/postgre-struct.go
@@ -0,0 +1,27 @@
+package postgrestruct
+
+type PostgresApp struct {
+	Id          int32
+	Status      string
+	Description string
+	ServerName  string
+	AdminUser   string
+	Password    string
+	Version     string
+	Environment string
+	SizeDisk    string
+	StorageType string
+	SizeCPU     string
+	SizeMemory  string
+}
+
+type ErrorResponse struct {
+	Message    string
+	StatusCode string
+}
+
+type SuccessResponse struct {
+	Message    string
+	StatusCode string
+	Status     string
+}
