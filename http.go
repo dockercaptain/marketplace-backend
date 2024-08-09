6f373b9b0549f6697ed76876cb51815b7148aa50
NaveenKashyapR <48998706+NaveenKashyapR@users.noreply.github.com>
2024-08-09 22:46:21 +0530

    Update http.go

--- http.go
+++ http.go
@@ -17,5 +17,6 @@ func main() {
 	mux.HandleFunc("POST /marketplace/apps/{appName}/create", pgController.CreateAppBasics)
 	mux.HandleFunc("GET /marketplace/apps/{appName}/{id}", pgController.GetApplicationById)
 	mux.HandleFunc("POST /marketplace/apps/{appName}/helmUpgrade", appController.AppHelmUpgrade)
+	mux.HandleFunc("GET /marketplace/apps/release/{appName}", appController.GetAppRelease)
 	http.ListenAndServe(":8080", mux)
 }
